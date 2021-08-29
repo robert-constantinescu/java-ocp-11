bdfb251c305f1e666f65b329f7a8454f0e63595c
Robert <constantinescu.robert94@gmail.com>
2021-08-29 15:29:25 +0300

    removed inner class from the Enum demo

--- src/cJavaObjectOrientedApproach/m7/demo/usingEnums/aircraft/Aircraft.java
+++ src/cJavaObjectOrientedApproach/m7/demo/usingEnums/aircraft/Aircraft.java
@@ -16,43 +16,6 @@ public class Aircraft {
         return modeADecimal;
     }
 
-    public String getModeAOctal() {
-        class ModeACode {
-            private String octal;
-
-            public ModeACode() {
-                int decimalValue = Aircraft.this.modeADecimal;
-
-                if (decimalValue < 0 || decimalValue > 7777) {
-                    this.octal = null;
-                }
-
-                String octalValue = Integer.toOctalString(decimalValue);
-
-                // Append leading zeros
-                if (octalValue.length() == 4) {
-                    this.octal = octalValue;
-                }
-                if (octalValue.length() == 3) {
-                    this.octal = "0" + octalValue;
-                }
-                if (octalValue.length() == 2) {
-                    this.octal = "00" + octalValue;
-                }
-                if (octalValue.length() == 1) {
-                    this.octal = "000" + octalValue;
-                }
-            }
-
-            public String getOctal() {
-                return octal;
-            }
-        }
-
-        ModeACode modeACode = new ModeACode();
-        return modeACode.getOctal();
-    }
-
     public String getModel() {
         return model;
     }
